b8e9c580b9c3615bd923f83cdc66193ffb571b7f
Eduardo Expósito Barrera <eduardoexba3@gmail.com>
2025-11-07 00:25:14 +0000

    fix: :bug: Fixing eslint

--- eslint.config.js
+++ eslint.config.js
@@ -8,7 +8,7 @@ export default [
   ...tseslint.configs.recommended,
   prettier,
   {
-    files: ["**/*.ts"],
+    files: ["src/*.ts"],
     languageOptions: {
       parser: tseslint.parser,
       parserOptions: {
